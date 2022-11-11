6ac4dfb291e6c9d52ca6e01493ca62f00a46261c
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-10 22:13:49 -0300

    commit

---
+++ _002-Loops/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) System.out.print(i + " ");
+
+        System.out.println();
+
+        int a = 9;
+        while( a >= 0) {
+            System.out.print(a + " ");
+            a--;
+        }
+
+        System.out.println();
+
+        int b = 1;
+        do{
+            System.out.print(b);
+        }while(b < 1);
+    }
+}
